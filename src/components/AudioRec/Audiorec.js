36d964f22f2f2cd5c9e9ab7ca2268d709e75c46c
RamakanthRGunishetty <ramakanthrg2003@gmail.com>
2023-03-19 17:09:36 +0530

    Helpline first view

--- src/components/AudioRec/Audiorec.js
+++ src/components/AudioRec/Audiorec.js
@@ -1,90 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
 import MicRecorder from 'mic-recorder-to-mp3'
 
 const Mp3Recorder = new MicRecorder({ bitRate: 128 })
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isRecording: false,
-      blobURL: '',
-      isBlocked: false,
-    }
-  }
 
-  start = () => {
-    if (this.state.isBlocked) {
+const AudioRec = () => {
+  const [isRecording, setIsRecording] = useState(false)
+  // const [blobURL, setBlobURL] = useState('')
+  const [isBlocked, setIsBlocked] = useState(false)
+
+  useEffect(() => {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log('Permission Granted')
+        setIsBlocked(false)
+      },
+      () => {
+        console.log('Permission Denied')
+        setIsBlocked(true)
+      },
+    )
+  }, [])
+
+  const startRecording = () => {
+    if (isBlocked) {
       console.log('Permission Denied')
     } else {
       Mp3Recorder.start()
         .then(() => {
-          this.setState({ isRecording: true })
+          setIsRecording(true)
         })
         .catch((e) => console.error(e))
     }
   }
 
-  stop = () => {
+  const stopRecording = () => {
     Mp3Recorder.stop()
       .getMp3()
       .then(([buffer, blob]) => {
-        const blobURL = URL.createObjectURL(blob)
-        this.setState({ blobURL, isRecording: false })
+        // const blobURL = URL.createObjectURL(blob)
+        // setBlobURL(blobURL)    //there
+        setIsRecording(false)
+
+        // Save the recorded file to the database here
+        // Example: fetch('/api/save-audio', { method: 'POST', body: blob })
       })
       .catch((e) => console.log(e))
   }
 
-  componentDidMount() {
-    navigator.getUserMedia(
-      { audio: true },
-      () => {
-        console.log('Permission Granted')
-        this.setState({ isBlocked: false })
-      },
-      () => {
-        console.log('Permission Denied')
-        this.setState({ isBlocked: true })
-      },
-    )
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <span>
-            {/* <button id="rec-mic">
-              Record now
-              
-            </button> */}
-          </span>
-          <span>
-            <button
-              id="rec-mic"
-              onClick={this.start}
-              disabled={this.state.isRecording}
-            >
-              Record
-              <span id="fa-mic">
-                <FontAwesomeIcon icon={faMicrophone} />
-              </span>
-            </button>
-            <button
-              id="rec-mic"
-              onClick={this.stop}
-              disabled={!this.state.isRecording}
-            >
-              Stop
-            </button>
-            <audio src={this.state.blobURL} controls="controls" />
-          </span>
-        </header>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>
+          {/* <button id='rec-mic'>
+            Record now
+          </button> */}
+        </span>
+        <span>
+          <button
+            id="rec-mic"
+            onClick={isRecording ? stopRecording : startRecording}
+            disabled={isBlocked}
+          >
+            {isRecording ? 'Stop' : 'Record'}
+            <span id="fa-mic">
+              <FontAwesomeIcon icon={faMicrophone} />
+            </span>
+          </button>
+          {/* <audio src={blobURL} controls="controls" /> there */}
+        </span>
+      </header>
+    </div>
+  )
 }
 
-export default App
+export default AudioRec
--- src/components/Helpline.js
+++ src/components/Helpline.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
-import './Helpline.css'
 import logo from '../images/trump.png'
 import { FaAddressCard } from 'react-icons/fa'
 
@@ -15,59 +14,73 @@ const Helpline = () => {
         Helpline
       </div>
       <br />
-      <div className="col-md-6">
-        <div className="Social">
-          <span className="info">
-            <h4>Info</h4>
-            Music is generally defined as the art of arranging sound to create
-            some combination of form, harmony, melody, rhythm or otherwise
-            expressive content.
-          </span>
+      <div className="row">
+        <div className="col-lg-5">
+          <Form className="form">
+            <div className="mb-3">
+              <Form.Group as={Col} controlId="formGridName">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  id="form_control"
+                  type="text"
+                  placeholder="Name"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formGridEmail">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  id="form_control"
+                  type="email"
+                  placeholder="Email"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formGridSubject">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  id="form_control"
+                  type="text"
+                  placeholder="Subject"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  id="form_control"
+                  as="textarea"
+                  rows="3"
+                  placeholder="Type your message"
+                />
+              </Form.Group>
+
+              <div className="mb-3">
+                <Form.Group controlId="formGridSubmit">
+                  <Form.Label></Form.Label>
+                  <Form.Control
+                    id="form_control_submit"
+                    type="submit"
+                    value={'Send'}
+                  />
+                </Form.Group>
+              </div>
+            </div>
+          </Form>
         </div>
-        <Form className="form">
-          <div className="mb-3">
-            <Form.Group as={Col} controlId="formGridName">
-              <Form.Label></Form.Label>
-              <Form.Control id="form_control" type="text" placeholder="Name" />
-            </Form.Group>
-
-            <img src={logo} alt="Logo" className="logo" />
-            <Form.Group controlId="formGridEmail">
-              <Form.Label></Form.Label>
-              <Form.Control
-                id="form_control"
-                type="email"
-                placeholder="Email"
-              />
-            </Form.Group>
+        <div className="col-lg-3">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="col-lg-3">
+          <div className="social">
+            <h4>Info</h4>
+            <p>
+              Music is generally defined as the art of arranging sound to create
+              some combination of form, harmony, melody, rhythm or otherwise
+              expressive content.
+            </p>
           </div>
-
-          <Form.Group className="mb-3" controlId="formGridSubject">
-            <Form.Label></Form.Label>
-            <Form.Control id="form_control" type="text" placeholder="Subject" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label></Form.Label>
-            <Form.Control
-              id="form_control"
-              as="textarea"
-              rows="3"
-              placeholder="Type your message"
-            />
-          </Form.Group>
-
-          <div className="mb-3">
-            <Form.Group controlId="formGridSubmit">
-              <Form.Label></Form.Label>
-              <Form.Control
-                id="form_control_submit"
-                type="submit"
-                value={'Send'}
-              />
-            </Form.Group>
-          </div>
-        </Form>
+        </div>
       </div>
     </div>
   )
