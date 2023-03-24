dda34fe69e6ea3c6e05af547bde26649b3256b72
Ramakanth R Gunishetty <93769233+RamakanthRGunishetty@users.noreply.github.com>
2023-03-24 19:10:36 +0530

    Frontline frontend responsive image

--- src/components/Frontline.js
+++ src/components/Frontline.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Frontline.css'
+import art from '../images/artists1.png'
 
 const Frontline = () => {
   return (
-    <div className="Frontline" id="Frontline" fluid>
+    <div className="Frontline" id="Frontline">
+      <div className="Artists">
+        <img src={art} alt="Art" className="art" />
+      </div>
       <div className="main-container">
         <div className="Intro">
           <span>Welcome</span>
           <span>to</span>
-          <span>Voice Maestro</span>
+          <span>VoiceMaestro</span>
         </div>
         <div className="btn-get">
-          <span>
-            <button className="btn-exp">Get Started</button>
-          </span>
+          <button className="btn-exp">Get Started</button>
         </div>
       </div>
     </div>
