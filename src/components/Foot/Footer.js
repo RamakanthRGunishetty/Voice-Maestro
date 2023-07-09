7f66c765843b57efc42144857e1c83b0eda78a68
Ramakanth R Gunishetty <93769233+RamakanthRGunishetty@users.noreply.github.com>
2023-07-09 12:41:38 +0530

    Footer with social media

--- src/components/Foot/Footer.js
+++ src/components/Foot/Footer.js
@@ -1,48 +1,71 @@
 import React from 'react'
 import './Footer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="music-note">
-          <div className="note"></div>
-          <div className="note"></div>
-          <div className="note"></div>
-          <div className="note"></div>
-        </div>
-        <div className="content">
-          <h3 className="footer-title">Music App</h3>
-          <nav className="footer-links">
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/about">About</a>
-              </li>
-              <li>
-                <a href="/services">Services</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="social-media">
-            <a href="https://www.facebook.com">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://www.twitter.com">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://www.instagram.com">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </div>
-        </div>
+    <div className="footer">
+      <div className="content">
+        <nav className="footer-links">
+          <ul>
+            <li>
+              <a href="#Frontline">Frontline</a>
+            </li>
+            <li>
+              <a href="#Infoline">Infoline</a>
+            </li>
+            <li>
+              <a href="#Workline">Workline</a>
+            </li>
+            <li>
+              <a href="#Helpline">Helpline</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </footer>
+      <div class="social-container">
+        <h3 className="follow">Follow us</h3>
+        <a
+          href="https://www.youtube.com/c/jamesqquick"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/learnbuildteach/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="https://www.twitter.com/jamesqquick"
+          className="twitter social"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a
+          href="https://www.instagram.com/learnbuildteach"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
+      <div
+        className="copy text-center p-1"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+      >
+        © 2023 Copyright:
+        <a className="text" href="#Frontline">
+          voicemaestro.com
+        </a>
+      </div>
+    </div>
   )
 }
 
